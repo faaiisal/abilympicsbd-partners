2916b575dc35d805ad393bf79a97d28bac269a1e
faaiisal <m.faisal674@gmail.com>
2026-09-23 00:23:22 +0600

    feat: Phase 3A Complete and Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import { About } from "@/components/About";
 import { CredibilityStats } from "@/components/CredibilityStats";
-import { CSID } from "@/components/CSID";
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import { Helsinki2027 } from "@/components/Helsinki2027";
 import { MetzJourney } from "@/components/MetzJourney";
-import { Participants } from "@/components/Participants";
+import { WhyPartner } from "@/components/WhyPartner";
 
 export default function Home() {
   return (
@@ -17,8 +16,7 @@ export default function Home() {
         <About />
         <MetzJourney />
         <Helsinki2027 />
-        <Participants />
-        <CSID />
+        <WhyPartner />
       </main>
     </>
   );
